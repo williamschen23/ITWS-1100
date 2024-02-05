5fcd3b4e6f711491bddc3e4de02411437a989d8b
williamschen23 <chenwill2005@gmail.com>
2024-02-05 11:11:07 -0500

    good progress so far

--- labs/lab03/README.md
+++ labs/lab03/README.md
@@ -1,2 +1,11 @@
 https://chenw23rpi.eastus.cloudapp.azure.com/iit/
 
+Websites that helped a lot:
+https://www.youtube.com/watch?v=AiQqip_pVbA
+
+Websites referenced:
+https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout/Flexbox
+https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe
+https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template
+https://www.w3schools.com/css/css3_flexbox.asp
+
--- resources/template.js
+++ resources/template.js
@@ -1,13 +1,35 @@
-class MyHeader extends HTMLElement {
-    connectCallback() {
+class Header extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
         this.innerHTML = `
-        <p>Hello</p>
-        <header>
-            <a href="./projects.html">Projects</a>
-            <a href="./labs/lab02/chenw23-WilliamsChen-resume.html">Resume</a>
-        </header>
+            <header id="Header">
+            <ul>
+                <li><a href="./labs/lab01/chenw23-lab1-AzureStatus.pdf">Lab 1</li>
+                <li><a href="./labs/lab02/chenw23-WilliamsChen-resume.html">Lab 2</li>
+                <li><a href="./labs/lab03/fileStructure.html">Lab 3</li>
+            </ul>
+            </header>
         `
     }
 }
 
-customElements.define('main-header', MyHeader)
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML = `
+            <footer id="Footer">
+                <a href="./projects.html">Projects</a>
+                <a href="./labs/lab02/chenw23-WilliamsChen-resume.html">Resume</a>
+                <p class="wee"></p>
+            </footer>
+        `
+    }
+}
+
+
+customElements.define('main-header', Header)
+customElements.define('main-footer', Footer)

845abc495e9e9036c2aa67a6a0a01ca47655977d
williamschen23 <chenwill2005@gmail.com>
2024-02-08 21:19:35 -0500

    Finished xml files

---
+++ labs/lab04/README.md
@@ -0,0 +1,9 @@
+http://chenw23rpi.eastus.cloudapp.azure.com/iit/labs/lab04/index.html
+
+
+1) https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml
+2) 
+
+
+I made some changes to the website to finally use templating.
+https://stackoverflow.com/questions/2255689/how-to-get-the-file-path-of-the-currently-executing-javascript-code
--- resources/template.js
+++ resources/template.js
@@ -4,12 +4,14 @@ class Header extends HTMLElement {
     }
     connectedCallback() {
         this.innerHTML = `
-            <header id="Header">
-            <ul>
-                <li><a href="./labs/lab01/chenw23-lab1-AzureStatus.pdf">Lab 1</li>
-                <li><a href="./labs/lab02/chenw23-WilliamsChen-resume.html">Lab 2</li>
-                <li><a href="./labs/lab03/fileStructure.html">Lab 3</li>
-            </ul>
+            <header class="Header">
+                <nav>
+                    <a href="../../index.html"><img src="../../resources/home-icon.png" alt="Home Button"></a>
+                    <p>Labs</p>
+                    <ul>
+                        <li><a href="../projects.html">Projects</a></li>
+                    </ul>
+                </nav>
             </header>
         `
     }
@@ -20,12 +22,15 @@ class Footer extends HTMLElement {
         super();
     }
     connectedCallback() {
+        let script = document.getElementsByTagName('script');
+        let src_js = script[script.length-1].src;
+        let src = src_js.substring(0, src_js.length-12);
         this.innerHTML = `
             <Footer class="Footer">
-                <p>Contacts: </p>
+                <p>${window.location.pathname}</p>
                 <ul>
-                    <li><a href="https://github.com/williamschen23"><img src="./resources/github-mark-white.png" alt="Home Button"></a><li>
-                    <li><a href="mailto:chenwill2005@gmail.com"><img src="./resources/gmail_icon.png" alt="Home Button"></a><li>
+                    <li><a href="https://github.com/williamschen23"><img src="${src}/github-mark-white.png" alt="Home Button"></a><li>
+                    <li><a href="mailto:chenwill2005@gmail.com"><img src="${src}/gmail_icon.png" alt="Home Button"></a><li>
                 </ul>
             </Footer>
         `

7062b90d3726697f162f8069f5a7b0c2738aa611
williamschen23 <chenwill2005@gmail.com>
2024-02-11 12:51:26 -0500

    completed lab04

--- labs/lab04/README.md
+++ labs/lab04/README.md
@@ -2,8 +2,14 @@ http://chenw23rpi.eastus.cloudapp.azure.com/iit/labs/lab04/index.html
 
 
 1) https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml
-2) 
+2) https://www.buzzfeed.com/tech.xml
+3) https://www.cnet.com/rss/iphone-update/
+4) https://www.latimes.com/business/technology/rss2.0.xml
+5) https://feeds.foxnews.com/foxnews/tech
 
 
 I made some changes to the website to finally use templating.
 https://stackoverflow.com/questions/2255689/how-to-get-the-file-path-of-the-currently-executing-javascript-code
+https://stackoverflow.com/questions/3151436/how-can-i-get-the-current-directory-name-in-javascript
+
+I learned how to use flexbox finally (and not using it excessively). To create the RSS and Atom files, if they had a namespace, i just deleted the line instead of including the namespace. Additionally, I created custom tags for the Atom elements.
--- resources/template.js
+++ resources/template.js
@@ -3,13 +3,34 @@ class Header extends HTMLElement {
         super();
     }
     connectedCallback() {
+        // current paththat calls the script
+        let path = window.location.pathname;
+
+        // takes out the path until the / 
+        // edge case: default / will just become nothing
+        let currDir = path.substring(0, path.lastIndexOf("/"));
+
+        // configuring what file we are in and what the root directory
+        // of the document is.
+        let fileStructure = "Home";
+        let rootDir = currDir;
+        if(path.includes("labs")) {
+            if(path.includes("projects")){
+                rootDir = currDir + "/..";
+                fileStructure = "Home → Projects"
+            } else {
+                rootDir = currDir + "/../..";
+                fileStructure = "Home → Projects → " + currDir.substring(currDir.lastIndexOf("/")+1);
+            }
+        }
+
         this.innerHTML = `
             <header class="Header">
                 <nav>
-                    <a href="../../index.html"><img src="../../resources/home-icon.png" alt="Home Button"></a>
-                    <p>Labs</p>
+                    <a href="${rootDir}/index.html"><img src="${rootDir}/resources/home-icon.png" alt="Home Button"></a>
+                    <p class=>${fileStructure}</p>
                     <ul>
-                        <li><a href="../projects.html">Projects</a></li>
+                        <li><a href="${rootDir}/labs/projects.html">Projects</a></li>
                     </ul>
                 </nav>
             </header>
@@ -22,17 +43,24 @@ class Footer extends HTMLElement {
         super();
     }
     connectedCallback() {
+        // takes the source directory of the script file, which is 
+        // a constant call to this file path
         let script = document.getElementsByTagName('script');
         let src_js = script[script.length-1].src;
-        let src = src_js.substring(0, src_js.length-12);
+        
+        // takes out the path to the last /
+        let src = src_js.substring(0, src_js.lastIndexOf("/"));
+
         this.innerHTML = `
-            <Footer class="Footer">
-                <p>${window.location.pathname}</p>
-                <ul>
-                    <li><a href="https://github.com/williamschen23"><img src="${src}/github-mark-white.png" alt="Home Button"></a><li>
-                    <li><a href="mailto:chenwill2005@gmail.com"><img src="${src}/gmail_icon.png" alt="Home Button"></a><li>
-                </ul>
-            </Footer>
+            <footer class="Footer">
+                <nav>
+                    <p><b>Contacts:</b></p>
+                    <ul>
+                        <li><a href="https://github.com/williamschen23"><img src="${src}/github-mark-white.png" alt="Home Button"></a><li>
+                        <li><a href="mailto:chenwill2005@gmail.com"><img src="${src}/gmail_icon.png" alt="Home Button"></a><li>
+                    </ul>
+                </nav>
+            </footer>
         `
     }
 }
